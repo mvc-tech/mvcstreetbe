af72aef3b7026b86d91872268dd8be78e3c4da48
l.doneddu <l.doneddu.mvc@gmail.com>
2023-12-20 10:24:36 +0100

    commit

---
+++ table2.php
@@ -0,0 +1,39 @@
+<?php 
+include_once "header.php";
+?>
+
+<div id="page-content-wrapper">
+    <!-- Page content-->
+    <div class="container-fluid">
+        <h1 class="mt-4">Controlli Aggregati</h1>
+    </div>
+
+    <div class="container-fluid">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Handle</th>
+                    <th scope="col">B</th>
+                    <th scope="col">C</th>
+                    <th scope="col">D</th>
+                </tr>
+            </thead>
+            <tbody>
+                   
+                <tr>
+                    <th scope="row">   </th>
+                    <td>   </td>
+                    <td>   </td>
+                    <td>   </td>
+                    <td>   </td>
+                    <td>   </td>
+                    <td>   </td>
+                </tr>
+
+            </tbody>
+        </table>
+    </div>
+</div>
