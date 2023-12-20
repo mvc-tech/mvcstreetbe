8a0154bf364cc0bbe44fb47d4bd1de367befd429
l.doneddu <l.doneddu.mvc@gmail.com>
2023-12-20 12:28:26 +0100

    api_insert

--- api/update_db.php
+++ api/update_db.php
@@ -1,11 +1,17 @@
 <?php
 include_once "conn.php";
 
-if(!isset($_GET['u'])) {
+if(!isset($_POST['u'])) {
     die('Syntax error: can\'t connect.');
 } else {
-    $token = $_GET['u'];
-    $data = $_GET['data'];
+    $token = $_POST['u'];
+    $data = $_POST['data'];
 
     $array = explode(',', $data);
+
+    $sql = "INSERT INTO controllo_singolo(id_macchina, zona, eccezione) values (?,?,?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("sss", $array['id_macchina'], $array['zona'], $array['eccezione']);
+    $stmt->execute();
+
 }
