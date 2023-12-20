d4d3cfa45fa6d99eb9a20436793b7c555dd7dd19
DonnyKKK <lorenzodoneddu03@gmail.com>
2023-12-20 12:08:00 +0100

    asd

---
+++ table_aggregati.php
@@ -0,0 +1,41 @@
+<?php 
+include_once "header.php";
+include_once "func/fetchdata.php";
+?>
+
+<div id="page-content-wrapper">
+    <!-- Page content-->
+    <div class="container-fluid">
+        <h1 class="mt-4">Analisi Aggregate</h1>
+    </div>
+
+    <div class="container-fluid">
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Fascia Oraria</th>
+                    <th scope="col">Numero Controlli</th>
+                    <th scope="col">Numero di Eccezioni</th>
+                    <th scope="col">ID Macchina</th>
+                    <th scope="col">Zona</th>
+                </tr>
+            </thead>
+            <tbody>
+                
+                    <?php foreach(fetchControlliAggregati($conn) as $controllo) :?>
+                        <tr>
+                        <td><?=$controllo['id']?></td>
+                        <td><?=$controllo['fascia']?></td>
+                        <td><?=$controllo['controlli']?></td>
+                        <td><?=$controllo['eccezioni']?></td>
+                        <td><?=$controllo['id_macchina']?></td>
+                        <td><?=$controllo['zona']?></td>
+                        </tr>
+
+                    <?php endforeach; ?>
+
+            </tbody>
+        </table>
+    </div>
+</div>
