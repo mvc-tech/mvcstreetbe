6a59355668b84e60ab172ad3b61571c087f1cda9
l.doneddu <l.doneddu.mvc@gmail.com>
2023-12-20 11:41:28 +0100

    editing

--- header.php
+++ header.php
@@ -21,20 +21,8 @@ include_once "res/conn.php";
                 <!-- Top navigation-->
                 <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
                     <div class="container-fluid">
-                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul class="navbar-nav ms-auto mt-2 mt-lg-0">
-                                <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-                                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                        <a class="dropdown-item" href="#!">Action</a>
-                                        <a class="dropdown-item" href="#!">Another action</a>
-                                        <div class="dropdown-divider"></div>
-                                        <a class="dropdown-item" href="#!">Something else here</a>
-                                    </div>
-                                </li>
-                            </ul>
-                        </div>
+                        <div class="navbar-brand"></div>
+                        <div class="spacer-height"></div>
                     </div>
                 </nav>
         <div class="d-flex" id="wrapper">
@@ -42,8 +30,8 @@ include_once "res/conn.php";
             <div class="border-end bg-white" id="sidebar-wrapper">
                 <div class="sidebar-heading border-bottom bg-light"><?=$SITE_TITLE?></div>
                 <div class="list-group list-group-flush">
-                    <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Tablet View</a>
-                    <a class="list-group-item list-group-item-action list-group-item-light p-3" href="table2.php">Analisi Aggregate</a>
-                    <a class="list-group-item list-group-item-action list-group-item-light p-3" href="table.php">Analisi Individuali</a>
+                    <a class="list-group-item list-group-item-action list-group-item-light p-3" href="tablet.php">Tablet View</a>
+                    <a class="list-group-item list-group-item-action list-group-item-light p-3" href="table_aggregati.php">Analisi Aggregate</a>
+                    <a class="list-group-item list-group-item-action list-group-item-light p-3" href="table_individuali.php">Analisi Individuali</a>
                 </div>
             </div>
